8ead0da0619c81e946e9af55f0b820daf30ea2a1
myrrh <myrrh@myrrh.ru>
2024-03-20 12:03:40 +0300

    Feat: доверстать раздел о туре, добавить анимацию кнопок

--- src/components/AboutTour/AboutTour.jsx
+++ src/components/AboutTour/AboutTour.jsx
@@ -4,12 +4,16 @@ import playButton from '../../images/video-icon.svg';
 export default function AboutTour() {
     return (
         <section className='about-tour'>
+            <div className='about-tour__video-container'>
             <picture className='about-tour__video about-tour__video_front'><button type='button' className='about-tour__play-button' /></picture>
             <picture href="#" className='about-tour__video about-tour__video_back'><button type='button' className='about-tour__play-button' /></picture>
+            </div>
+            <div className='about-tour__info'>
             <h1 className='main__title'>о нашем походе</h1>
             <h2 className='main__subtitle'>Исследуйте все горные массивы мира вместе с нами</h2>
             <p className='main__paragraph'>Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа Hampden-Sydney, штат Вирджиния, взял одно из самых странных слов в Lorem Ipsum, "consectetur"и занялся его поисками в классической латинской литературе.</p>
             <button className='main__button'>Программа тура</button>
+            </div>
         </section>
     );
 }
