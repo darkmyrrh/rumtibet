5f1b602d2405c7e65ad37b8f71171c11061f8800
myrrh <myrrh@myrrh.ru>
2024-05-14 12:17:56 +0300

    Feat: сверстать контрольные точки для раздела подписки

--- src/components/Subscription/Subscription.jsx
+++ src/components/Subscription/Subscription.jsx
@@ -9,7 +9,6 @@ export default function Subscription() {
             <form className='subscription__form'>
                 <label htmlFor='email' className='subscription__form-label'>Введите e-mail адрес</label>
                 <input type='email' name='email' id='email' placeholder='name@domain.com' className='subscription__form-input' />
-
                 <button type='submit' aria-label='Подписаться' className='main__button subscription__form-submit'>Подписаться на новости</button>
                 <p className='subscription__form-text'>подписываясь на новости, вы автоматически соглашаетесь с <a href="#" className='subscription__form-link'>условиями обработки персональных данных</a> и <a href="#" className='subscription__form-link'>правилами рекламных рассылок</a></p>
             </form>
